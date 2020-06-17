64c46a53c498aadce70f685bb81a8a88bf09ded1
Emily Braman <embraman10@gmail.com>
2020-06-17 19:55:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,6 +146,7 @@ def shoe_size (player_shoe)
 def team_colors (team_col)
   game_hash.each do |team, team_nam|
     if team_nam[:team_name].each do |jersey|
+      binding.pry
       jersey[:team_colors] == team_col
       binding.pry
     end
