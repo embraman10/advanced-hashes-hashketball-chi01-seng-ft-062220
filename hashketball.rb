32fde23c1d98106e479145342f7acef98191a724
Emily Braman <embraman10@gmail.com>
2020-06-17 19:51:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -144,13 +144,11 @@ def shoe_size (player_shoe)
 
 
 def team_colors (team_col)
-  jersey = " "
   game_hash.each do |team, team_nam|
-    if team_nam[:team_name] == team_col
-        jersey = team_nam[:team_colors]
+    if team_nam[:team_name] do |jersey|
+        team_col[:team_colors] == team_col
       end
     end
-    jersey
 end
 
 
