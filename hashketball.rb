abdcf78050a45650d3a258cdc06fd3e0172d7e68
Emily Braman <embraman10@gmail.com>
2020-06-17 18:29:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,3 +117,15 @@ def game_hash
     }
   }
 end
+
+def num_points_scored (player_points)
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player [:player_name] == player_points
+          end
+        end
+      end
+    end
+end
